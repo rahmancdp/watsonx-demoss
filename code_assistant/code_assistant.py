e2ceb5018055f508a8fb6733f178d658eff46f58
Ying Kit WONG <yingkitw@yings-mbp.kr.ibm.com>
2023-11-30 10:26:34 +0800

    add explaination

--- code_assistant/code_assistant.py
+++ code_assistant/code_assistant.py
@@ -36,13 +36,23 @@ params = GenerateParams(
     top_p=1
 )
 
-llm = Model(model="bigcode/starcoder",credentials=creds, params=params)
+llmstarcoder = Model(model="bigcode/starcoder",credentials=creds, params=params)
+llmllama = Model(model="meta-llama/llama-2-70b-chat",credentials=creds, params=params)
 
-def buildprompt(code,sourcelang,targetlang):
+def buildpromptforconvert(code,sourcelang,targetlang):
     return f"""translate the following code from {sourcelang} to {targetlang}:
 {sourcelang}:
 {code}
 {targetlang}:"""
+
+def buildpromptfordocumentation(code,sourcelang):
+    return f"""[INST]
+please generate documentation for the {sourcelang} source code in backquoted
+<<SYS>>
+`{code}`
+<<SYS>>
+[/INST]
+documentation:"""
 
 def codeconvert(incode,sourcelang,targetlang):
     #chunking
@@ -53,174 +63,53 @@ def codeconvert(incode,sourcelang,targetlang):
 
     prompts = []
     for chunk in chunks:
-        prompts += [buildprompt(chunk,sourcelang,targetlang)]
+        prompts += [buildpromptforconvert(chunk,sourcelang,targetlang)]
     code = ""
-    for response in llm.generate_async(prompts,ordered=True):
+    for response in llmstarcoder.generate_async(prompts,ordered=True):
         code += response.generated_text
     return code
 
+def codedocumentation(incode,sourcelang):
+    #chunking
+    chunk_size = len(incode)//4
+    chunks = []
+    for i in range(0, len(incode), chunk_size):
+        chunks += [incode[i:i+chunk_size]]
+
+    prompts = []
+    for chunk in chunks:
+        prompts += [buildpromptfordocumentation(chunk,sourcelang)]
+    documentation = ""
+    for response in llmllama.generate_async(prompts,ordered=True):
+        documentation += response.generated_text
+    return documentation
+
 temp_dir = tempfile.TemporaryDirectory()
 
-st.header("code converter powered by watsonx")
+st.set_page_config(layout="wide")
+st.header("code helper powered by watsonx")
 
-incode = '''
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Common;
-using System.Runtime.Serialization;
-using TDSService.BaseClassLibrary;
+incode = ""
+outcode = ""
+outdocumenation = ""
 
-namespace TDSService.SAL01BusinessEntities.CP.JF;
+colsource, colconvert, coldocumentation = st.columns(3)
 
-[DataContract(Namespace = "http://TDSService.DataContracts/2007/08", Name = "AakRebateApplicationCRep")]
-public class AakRebateApplicationCRep : DataAccessBase<AakRebateApplicationC>
-{
-    public AakRebateApplicationCRep(Session session)
-        : base(session)
-    {
-    }
-
-    protected override void BuildMapping()
-    {
-        base.ORMappingVar.TableMapping("salb409C");
-        base.ORMappingVar.AddColumnMapping("Id", "Id", true, "s_salb409");
-        base.ORMappingVar.AddColumnMapping("DTstamp", "DTstamp");
-        base.ORMappingVar.AddColumnMapping("DOperatordate", "DOperatordate");
-        base.ORMappingVar.AddColumnMapping("NKaid", "NKaid");
-        base.ORMappingVar.AddColumnMapping("NDealerid", "NDealerid");
-        base.ORMappingVar.AddColumnMapping("NCompanyid", "NCompanyid");
-        base.ORMappingVar.AddColumnMapping("NOperatorid", "NOperatorid");
-        base.ORMappingVar.AddColumnMapping("NCompanyrebatemvalue", "NCompanyrebatemvalue");
-        base.ORMappingVar.AddColumnMapping("NRebatemamt", "NRebatemamt");
-        base.ORMappingVar.AddColumnMapping("NRebatemvalue", "NRebatemvalue");
-        base.ORMappingVar.AddColumnMapping("VCompanyremark", "VCompanyremark");
-        base.ORMappingVar.AddColumnMapping("VBillno", "VBillno");
-        base.ORMappingVar.AddColumnMapping("VAuditinglevel", "VAuditinglevel");
-        base.ORMappingVar.AddColumnMapping("VManagersuggestion", "VManagersuggestion");
-        base.ORMappingVar.AddColumnMapping("VMinistersuggestion", "VMinistersuggestion");
-        base.ORMappingVar.AddColumnMapping("VRebatemethod", "VRebatemethod");
-        base.ORMappingVar.AddColumnMapping("VOrgremark", "VOrgremark");
-        base.ORMappingVar.AddColumnMapping("VDealerremark", "VDealerremark");
-        base.ORMappingVar.AddColumnMapping("VFinishstate", "VFinishstate");
-        base.ORMappingVar.AddColumnMapping("VGeneralsuggestion", "VGeneralsuggestion");
-        base.ORMappingVar.AddColumnMapping("VAssistantsuggestion", "VAssistantsuggestion");
-        base.ORMappingVar.AddColumnMapping("NSdcaudit", "NSdcaudit");
-        base.ORMappingVar.AddColumnMapping("NDkhywyaudit", "NDkhywyaudit");
-        base.ORMappingVar.AddColumnMapping("NKfsjlaudit", "NKfsjlaudit");
-        base.ORMappingVar.AddColumnMapping("NChsjlaudit", "NChsjlaudit");
-        base.ORMappingVar.AddColumnMapping("NBzaudit", "NBzaudit");
-        base.ORMappingVar.AddColumnMapping("DSdcauditdate", "DSdcauditdate");
-        base.ORMappingVar.AddColumnMapping("DDkhywyauditdate", "DDkhywyauditdate");
-        base.ORMappingVar.AddColumnMapping("DKfsjlauditdate", "DKfsjlauditdate");
-        base.ORMappingVar.AddColumnMapping("DChsjlauditdate", "DChsjlauditdate");
-        base.ORMappingVar.AddColumnMapping("DBzauditdate", "DBzauditdate");
-        base.ORMappingVar.AddColumnMapping(false, "VFinishDesc", "VFinishDesc");
-        base.ORMappingVar.AddColumnMapping(false, "VOperatorCode", "VOperatorCode");
-        base.ORMappingVar.AddColumnMapping(false, "VOperatorName", "VOperatorName");
-        base.ORMappingVar.AddColumnMapping(false, "VDealer", "VDealer");
-        base.ORMappingVar.AddColumnMapping(false, "VDealerText", "VDealerText");
-        base.ORMappingVar.AddColumnMapping(false, "VKacode", "VKacode");
-        base.ORMappingVar.AddColumnMapping(false, "VKaname", "VKaname");
-        base.ORMappingVar.AddColumnMapping(false, "VAuditinglevelcode", "VAuditinglevelcode");
-        base.ORMappingVar.AddColumnMapping(false, "VGrade", "VGrade");
-        base.ORMappingVar.AddColumnMapping(false, "VRebatemethodcode", "VRebatemethodcode");
-        base.ORMappingVar.AddColumnMapping(false, "Vkalevel", "Vkalevel");
-        base.ORMappingVar.AddColumnMapping(false, "VKalevelcode", "VKalevelcode");
-        base.ORMappingVar.AddColumnMapping(false, "VOrgantext", "VOrgantext");
-        base.ORMappingVar.AddColumnMapping(false, "Vtext1", "Vtext1");
-        base.ORMappingVar.AddColumnMapping(false, "Vtext2", "Vtext2");
-        base.ORMappingVar.AddColumnMapping(false, "Vtext3", "Vtext3");
-        base.ORMappingVar.AddColumnMapping(false, "Vtext4", "Vtext4");
-        base.ORMappingVar.AddColumnMapping(false, "Vtext5", "Vtext5");
-        base.ORMappingVar.AddColumnMapping(false, "VTextcode1", "VTextcode1");
-        base.ORMappingVar.AddColumnMapping(false, "VTextcode2", "VTextcode2");
-        base.ORMappingVar.AddColumnMapping(false, "VTextcode3", "VTextcode3");
-        base.ORMappingVar.AddColumnMapping(false, "VTextcode4", "VTextcode4");
-        base.ORMappingVar.AddColumnMapping(false, "VTextcode5", "VTextcode5");
-        base.ORMappingVar.QuerySQL = "\r\nselect a.* from salb409c a\r\n";
-    }
-
-    public override AakRebateApplicationC NewBusinessEntity()
-    {
-        return new AakRebateApplicationC();
-    }
-
-    public List<AakRebateApplicationC> GetAakRebateApplicationCList(string vFlag, string vFinishstate, string dOperatorBegin, string dOperatorEnd, decimal nOrgid)
-    {
-        return base.FindMany("p_sal91_search_salb409c", new object[11]
-        {
-            vFlag,
-            vFinishstate,
-            dOperatorBegin,
-            dOperatorEnd,
-            nOrgid,
-            ((BaseClass)this).GetCompanyId(),
-            ((BaseClass)this).GetOperatorId(),
-            ((BaseClass)this).GetLanguageCode(),
-            0,
-            "prm_msg",
-            "out_cursor"
-        });
-    }
-
-    public bool DealAakRebateApplicationC(AakRebateApplicationC beInfo, string vNewstatus, string vOldstatus)
-    {
-        DbCommand storedProcCommand = base.DB.GetStoredProcCommand("p_sal91_update_salb409c");
-        base.DB.AddInParameter(storedProcCommand, "p_docid", DbType.Decimal, (object)beInfo.Id);
-        base.DB.AddInParameter(storedProcCommand, "n_operator", DbType.Decimal, (object)((BaseClass)this).GetOperatorId());
-        base.DB.AddInParameter(storedProcCommand, "n_companyid", DbType.Decimal, (object)((BaseClass)this).GetCompanyId());
-        base.DB.AddInParameter(storedProcCommand, "v_language", DbType.String, (object)((BaseClass)this).GetLanguageCode());
-        base.DB.AddInParameter(storedProcCommand, "v_Oldstatus", DbType.String, (object)vOldstatus);
-        base.DB.AddInParameter(storedProcCommand, "v_Newstatus", DbType.String, (object)vNewstatus);
-        base.DB.AddOutParameter(storedProcCommand, "prm_code", DbType.Decimal, 0);
-        base.DB.AddOutParameter(storedProcCommand, "prm_msg", DbType.String, 65535);
-        base.DB.ExecuteNonQuery(storedProcCommand);
-        int result = 0;
-        if (base.DB.GetParameterValue(storedProcCommand, "prm_code") != null && !string.IsNullOrEmpty(base.DB.GetParameterValue(storedProcCommand, "prm_code").ToString()))
-        {
-            int.TryParse(base.DB.GetParameterValue(storedProcCommand, "prm_code").ToString(), out result);
-        }
-        string text = string.Empty;
-        if (base.DB.GetParameterValue(storedProcCommand, "prm_msg") != null && !string.IsNullOrEmpty(base.DB.GetParameterValue(storedProcCommand, "prm_msg").ToString()))
-        {
-            text = base.DB.GetParameterValue(storedProcCommand, "prm_msg").ToString();
-        }
-        if (result != 0)
-        {
-            ((BaseClass)this).ThrowWarning(result, PublicFun.StringToArry(text));
-            return false;
-        }
-        return true;
-    }
-
-    public List<AakRebateApplicationC> GetAakRebateApplicationCById(decimal nId)
-    {
-        base.ORMappingVar.QuerySQL = "\r\n      select a.*,\r\n             b.vdealer vDealer,\r\n             c.vlongtext   VDealerText,\r\n             d.vcode   VOperatorCode,\r\n             d.vtext   VOperatorName,\r\n             e.vtext   VFinishDesc,\r\n             f.vkacode,\r\n             f.vkaname,\r\n             g.vtext vkalevel,\r\n             h.vtext VRebatemethodcode,\r\n             f.vgrade,\r\n             i.vtext VAuditinglevelcode,\r\n             a3.vtext vOrgantext,\r\n             i2.vtext vtext1,\r\n             i2.vcode VTextcode5,\r\n             j.vtext vtext2,\r\n             k.vtext vtext3,\r\n             l.vtext vtext4,\r\n             m.vtext vtext5,\r\n             j.vcode VTextcode1,\r\n             k.vcode VTextcode2,\r\n             l.vcode VTextcode3,\r\n             m.vcode VTextcode4\r\n        from salb409c a\r\n        left join mdac100 b\r\n          on a.ndealerid = b.id\r\n        left join sysc000_m c\r\n          on b.id = c.nmainid\r\n         and c.vlanguagecode = '#0#'\r\n        left join sysc000_m d\r\n          on a.noperatorid = d.nmainid\r\n         and d.vlanguagecode = '#0#'\r\n        left join v_sysc009d e\r\n          on a.vfinishstate = e.vitemcode\r\n         and e.vdictcode = 'KAREBATESTATUS'\r\n         and e.ncompanyid = #1#\r\n        left join salb405c f\r\n        on a.nkaid=f.id\r\n        left join v_sysc009d g\r\n        on f.vkalevel=g.vitemcode\r\n        and g.vdictcode='SPCUSLEVEL'\r\n        and g.ncompanyid = #1#\r\n        left join v_sysc009d h\r\n        on a.vrebatemethod=h.vitemcode\r\n        and h.vdictcode='REBATEVALUEMETHOD'\r\n        and h.ncompanyid=#1#\r\n        left join v_sysc009d i\r\n        on a.vauditinglevel = i.vitemcode\r\n        and i.vdictcode = 'KAAUDITLEVEL'\r\n        and i.ncompanyid = #1#\r\n         left join v_dlr_sal a2 on a.ndealerid=a2.id\r\n         left join sysc000_m a3 on a2.NORGAN=a3.nmainid and a3.vlanguagecode='#0#'\r\n         left join sysc000_m i2 on a.nsdcaudit=i2.nmainid and i2.vlanguagecode='#0#'\r\n         left join sysc000_m j on a.ndkhywyaudit=j.nmainid and j.vlanguagecode='#0#'\r\n         left join sysc000_m k on a.nkfsjlaudit =k.nmainid and k.vlanguagecode='#0#'\r\n         left join sysc000_m l on a.nchsjlaudit =l.nmainid and l.vlanguagecode='#0#'\r\n         left join sysc000_m m on a.nbzaudit=m.nmainid and m.vlanguagecode='#0#' \r\n     where a.ncompanyid=#1# and a.id='#2#'and a.vfinishstate not in(90,99) \r\n";
-        string[] querySqlParams = new string[3]
-        {
-            ((BaseClass)this).GetLanguageCode(),
-            ((BaseClass)this).GetCompanyId().ToString(),
-            Convert.ToString(nId)
-        };
-        base.ORMappingVar.SetQuerySqlParams(querySqlParams);
-        return base.FindMany("");
-    }
-}
-'''
-
-convertbutton = st.button("convert")
-
-col1, col2 = st.columns(2)
-
-with col1:
+with colsource:
+    uploaded_file = st.file_uploader(label="upload a source code file",type=['cs','java','py','c','cpp'])
+    if uploaded_file is not None:
+        st.write("filename:", uploaded_file.name)
+        with open(os.path.join(pathlib.Path(temp_dir.name),uploaded_file.name),"wb") as f:
+            f.write(uploaded_file.getbuffer())
+            incode = uploaded_file.read().decode()
     sourcelang = st.selectbox(
         'What program language you want to convert from?',
         ('csharp','java', 'python'))
     sourcecode = st.code(incode,language=sourcelang)
     # sourcecode = code_editor(incode, lang=sourcelang)
 
-with col2:
+with colconvert:
+    convertbutton = st.button("convert")
     targetlang = st.selectbox(
         'What program language you want to convert to?',
         ('java', 'csharp', 'python'))
@@ -228,3 +117,9 @@ with col2:
         # st.write(sourcecode['text'])
         outcode = codeconvert(incode,sourcelang,targetlang)
         st.code(outcode,language=targetlang)
+
+with coldocumentation:
+    docbutton = st.button("generate documentation")
+    if docbutton:
+        outdocumenation = codedocumentation(incode,sourcelang)
+        st.write(outdocumenation)
