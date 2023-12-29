00441bcacc5fe8a36a8f6e20a82dc291834c3b91
Ying Kit WONG <yingkitw@192.168.0.101>
2023-12-29 21:51:43 +0800

    partially fix

--- file_translator/file_translator.py
+++ file_translator/file_translator.py
@@ -4,62 +4,69 @@ from pptx.enum.lang import MSO_LANGUAGE_ID
 
 from docx import Document
 
-from genai.credentials import Credentials
-from genai.schemas import GenerateParams
-from genai.model import Model
+from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
+from ibm_watson_machine_learning.foundation_models import Model
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watson_machine_learning.foundation_models.extensions.langchain import WatsonxLLM
 
 import tempfile
 import pathlib
+import json
+import ast
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = st.secrets["GENAI_KEY"]
-api_endpoint = st.secrets["GENAI_API"]
+api_key = st.secrets["API_KEY"]
+project_id = st.secrets["PROJECT_ID"]
 
-api_key = os.getenv("GENAI_KEY", None)
-api_endpoint = os.getenv("GENAI_API", None)
+api_key = os.getenv("API_KEY", None)
+project_id = os.getenv("PROJECT_ID", None)
 
-creds = Credentials(api_key,api_endpoint)
+creds = {
+    "url"    : "https://us-south.ml.cloud.ibm.com",
+    "apikey" : api_key
+}
 
-params = GenerateParams(
-    decoding_method="greedy",
-    max_new_tokens=1000,
-    min_new_tokens=1,
-    stream=False,
-    top_k=50,
-    top_p=1,
-    stop_sequences=["\\n\\n"]
-)
+params = {
+    GenParams.DECODING_METHOD:"greedy",
+    GenParams.MAX_NEW_TOKENS:3000,
+    GenParams.MIN_NEW_TOKENS:1,
+    GenParams.TOP_K:50,
+    GenParams.TOP_P:1,
+    # GenParams.STOP_SEQUENCES:["\\n\\n"],
+}
 
-llm = Model(model="meta-llama/llama-2-13b-chat",credentials=creds, params=params)
+llm = Model("meta-llama/llama-2-13b-chat",creds, params,project_id)
 
 def buildprompt(text,sourcelang,targetlang):
-    return f"""[INST]be a translator, be concise.
-    return the translated content only.
-    please help translate following {sourcelang} in backquoted to {targetlang}:
-    <<SYS>>
-    {sourcelang}:`{text}`
-    <<SYS>>
+    prompt = f"""[INST]you are a translator, and need to process a json file.
+    -for each field in json, translate the field value in {sourcelang} language to {targetlang} language:
+    -dont translate the field name.
+    -dont show note.
+    -show the json only.
     [/INST]
-    {targetlang}:"""
+    {sourcelang} json:{text}
+    {targetlang} json:"""
+    # print(prompt)
+    return prompt
 
 def translate_doc(docfile,sourcelang,targetlang):
     adoc = Document(docfile)
 
     for paragraph in adoc.paragraphs:
         prompt = buildprompt(paragraph.text,sourcelang,targetlang)
-        for response in llm.generate([prompt]):
-            paragraph.text = response.generated_text
+        for response in llm.generate_text([prompt]):
+            paragraph.text = response
 
     for table in adoc.tables:
         for row in table.rows:
             for cell in row.cells:
                 prompt = buildprompt(cell.text,sourcelang,targetlang)
-                for response in llm.generate([prompt]):
-                    cell.text = response.generated_text
+                for response in llm.generate_text([prompt]):
+                    cell.text = response
 
     output_file_path = docfile.replace('.docx', f'-{targetlang}.docx')
     adoc.save(output_file_path)
@@ -78,30 +85,54 @@ def translate_ppt(pptfile,sourcelang,targetlang):
         st.write(f'Slide {slide_number} of {len(presentation.slides)}')
         slide_number += 1
 
+        context = {}
+
         # translate comments
         if slide.has_notes_slide:
             text_frame = slide.notes_slide.notes_text_frame
             if len(text_frame.text) > 0:
-                prompt = buildprompt(text_frame.text,sourcelang,targetlang)
-                for response in llm.generate([prompt]):
-                    slide.notes_slide.notes_text_frame.text = response.generated_text
+                context[text_frame.text] = text_frame.text
+
+        # translate other texts
+        for shape in slide.shapes:
+            if shape.has_table:
+                for cell in shape.table.iter_cells():
+                    context[cell.text] = cell.text
+
+            if shape.has_text_frame:
+                for paragraph in shape.text_frame.paragraphs:
+                    for index, paragraph_run in enumerate(paragraph.runs):
+                        context[paragraph_run.text] = paragraph_run.text
+
+        # print(context)
+
+        prompt = buildprompt(str(context),sourcelang,targetlang)
+        allresp = ""
+        for response in llm.generate_text([prompt]):
+            print(response)
+            allresp += response
+        print(allresp)
+        
+        contextout = ast.literal_eval(allresp)
+
+        # translate comments
+        if slide.has_notes_slide:
+            text_frame = slide.notes_slide.notes_text_frame
+            if len(text_frame.text) > 0:
+                slide.notes_slide.notes_text_frame.text = contextout[text_frame.text]
 
 
         # translate other texts
         for shape in slide.shapes:
             if shape.has_table:
                 for cell in shape.table.iter_cells():
-                    prompt = buildprompt(cell.text,sourcelang,targetlang)
-                    for response in llm.generate_async([prompt]):
-                        cell.text = response.generated_text
+                    cell.text = contextout[cell.text]
 
             if shape.has_text_frame:
                 for paragraph in shape.text_frame.paragraphs:
                     for index, paragraph_run in enumerate(paragraph.runs):
-                        prompt = buildprompt(paragraph_run.text,sourcelang,targetlang)
-                        for response in llm.generate([prompt]):
-                            paragraph.runs[index].text = response.generated_text
-                            # paragraph.runs[index].font.language_id = targetlang
+                        paragraph.runs[index].text = contextout[paragraph_run.text]
+                        # paragraph.runs[index].font.language_id = targetlang
 
     output_file_path = pptfile.replace('.pptx', f'-{targetlang}.pptx')
     presentation.save(output_file_path)
@@ -113,11 +144,11 @@ st.header("document translator powered by watsonx")
 
 sourcelang = st.selectbox(
     'What language you want to translate from?',
-    ('English','Chinese', 'Korean', 'Japanese','Thai','Malay','Bahasa Indonesia'))
+    ('English','中文', 'Korean', 'Japanese','Thai','Malay','Bahasa Indonesia'))
 
 targetlang = st.selectbox(
     'What language you want to translate to?',
-    ('Chinese', 'Korean', 'Japanese','Thai','Malay','Bahasa Indonesia','English'))
+    ('中文', 'Korean', 'Japanese','Thai','Malay','Bahasa Indonesia','English'))
 
 
 uploaded_file = st.file_uploader(label="upload a ppt",type=['ppt','pptx','doc','docx'])
