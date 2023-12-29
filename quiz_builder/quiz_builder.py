af6febfc3b351de0f5fb959879ec3e1aa9946b8d
Ying Kit WONG <yingkitw@192.168.0.101>
2023-12-29 16:49:26 +0800

    add quiz

--- quiz_builder/quiz_builder.py
+++ quiz_builder/quiz_builder.py
@@ -3,6 +3,7 @@ import logging
 import os
 import tempfile
 import pathlib
+import json
 
 import streamlit as st
 from dotenv import load_dotenv
@@ -10,9 +11,10 @@ from dotenv import load_dotenv
 from langchain.document_loaders import PyPDFLoader
 from sentence_transformers import SentenceTransformer
 
-from genai.credentials import Credentials
-from genai.schemas import GenerateParams
-from genai.model import Model
+from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
+from ibm_watson_machine_learning.foundation_models import Model
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watson_machine_learning.foundation_models.extensions.langchain import WatsonxLLM
 
 from typing import Literal, Optional, Any
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -80,24 +82,25 @@ st.header("quiz builder")
 
 load_dotenv()
 
-api_key = st.secrets["GENAI_KEY"]
-api_endpoint = st.secrets["GENAI_API"]
+api_key = st.secrets["API_KEY"]
+project_id = st.secrets["PROJECT_ID"]
 
-api_key = os.getenv("GENAI_KEY", None)
-api_endpoint = os.getenv("GENAI_API", None)
+api_key = os.getenv("API_KEY", None)
+project_id = os.getenv("PROJECT_ID", None)
 
-# handler = StdOutCallbackHandler()
+creds = {
+    "url"    : "https://us-south.ml.cloud.ibm.com",
+    "apikey" : api_key
+}
 
-creds = Credentials(api_key,api_endpoint)
-
-params = GenerateParams(
-    decoding_method="greedy",
-    max_new_tokens=1000,
-    min_new_tokens=1,
-    # stream=True,
-    top_k=50,
-    top_p=1
-)
+params = {
+    GenParams.DECODING_METHOD:"sample",
+    GenParams.MAX_NEW_TOKENS:1000,
+    GenParams.MIN_NEW_TOKENS:1,
+    GenParams.TEMPERATURE:0.7,
+    GenParams.TOP_K:50,
+    GenParams.TOP_P:1,
+}
 
 # Sidebar contents
 with st.sidebar:
@@ -128,43 +131,45 @@ def read_push_embeddings(docs):
 
 def buildquiz(informations, topic):
     prompt = f"""[INST]base on the topic and informations provided, 
-    generate a question along with multiple choice quiz
+    generate a scenario base question along with multiple choice quiz
+    assume a role in the question, and assign a specific task to the role, and ask which answer best address the task.
+    the answer should describe a operation very detail.
+    the answer options should be specific, descriptive, and with more detail about the technique.
     mark the answer and provide explaination.
-    - create newline for each question and option.
+    <<SYS>>
+    notes:
+    - one paragraph per one answer option.
     - please follow the layout provided in backquoted.
-    - question should be scenario bases, descriptive, detail enough.
-    - answer should be specific, descriptive, detail enough.
     - ensure the answer options be different, but similar enough that the user hard to determine.
     - ensure only one answer option be correct.
     - explain the correct answer as well as the incorrect answer options.
     - output in markdown.
-    <<SYS>>
     topic:{topic}
     informations:
     {informations}
     layout: `question?
 
-    a) answer option.
-    b) answer option.
-    c) answer option.
-    d) answer option.
+    a) answer option.\n
+    b) answer option.\n
+    c) answer option.\n
+    d) answer option.\n
 
     correct answer (option), explaination.`
-    <<SYS>>
+    <</SYS>>
     [/INST]
     markdown quiz:"""
 
     prompts = [prompt]
     answer = ""
-    for response in model.generate_async(prompts,ordered=True):
-        answer += response.generated_text
+    for response in model.generate_text(prompts):
+        answer += response
     return answer
 
 docs = read_pdf(uploaded_files)
 if docs is not None:
     db = read_push_embeddings(docs)
 
-model = Model(model="meta-llama/llama-2-70b-chat",credentials=creds, params=params)
+model = Model("meta-llama/llama-2-70b-chat",creds, params, project_id)
 
 history = []
 
@@ -187,9 +192,9 @@ if topic := st.chat_input("your topic"):
     st.session_state.messages.append({"role": "user", "content": topic})
     with st.spinner(text="building...", cache=False):
         docs = db.similarity_search(topic)
-        answer = buildquiz(docs, topic)
+        answer = buildquiz(docs,topic)
+        # print(answer)
+        st.session_state.messages.append({"role": "agent", "content": answer}) 
 
-    st.session_state.messages.append({"role": "agent", "content": answer}) 
-
-    with st.chat_message("agent"):
-        st.markdown(answer)
+        with st.chat_message("agent"):
+            st.markdown(answer)
